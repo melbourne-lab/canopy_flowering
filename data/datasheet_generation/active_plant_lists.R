2cf9cd4833c3f0b4739aef7089e657e426dce637
swnordstrom <swnordstrom@email.wm.edu>
2020-06-24 14:48:06 -0600

    fix active plant list code to (1) include can't find plants and one-done plants for june 24 active plant list and (2) write csv for june 24 data entry

--- data/datasheet_generation/active_plant_lists.R
+++ data/datasheet_generation/active_plant_lists.R
@@ -49,12 +49,12 @@ read.csv('data/data_active.csv') %>%
   select(-X) %>%
   rbind(read.csv('data/data_entry_06-21-2020.csv')) %>%
   # Give only plants with non-zero columns and/or yellow visible in notes
-  filter(Infl_spread > 0 | Infl_done > 0 | grepl('yv', Note)) %>%
+  filter(Infl_spread > 0 | Infl_done > 0 | grepl('yv', Note) | grepl("can't find", Note)) %>%
   # Format date
   mutate(Date = as.Date(Date, '%m/%d/%y')) %>%
   # Get rid of plants which end with two days of no spread flowers
   group_by(Plot, Tag) %>% arrange(Plot, Tag, Date) %>% 
-  filter(!(Infl_spread[1] | !Infl_spread[2])) %>% ungroup() %>%
+  filter(!(Infl_spread[1] & !Infl_spread[2])) %>% ungroup() %>%
   # Remove duplicates
   arrange(Plot, Tag, desc(Date)) %>%
   distinct(Plot, Tag, .keep_all = TRUE) %>%
@@ -76,4 +76,20 @@ read.csv('data/data_active.csv') %>%
   write.csv('data/datasheet_generation/datasheet_outputs/active_plas_2020-06-24.csv',
             row.names = FALSE, na = '')
 
+# Prepare a csv for data entry
+read.csv('data/datasheet_generation/datasheet_outputs/active_plas_2020-06-24.csv') %>%
+  select(Date, Pl, Tag) %>%
+  rename(Plot = Pl) %>%
+  mutate(Page = NA,
+         Date = NA,
+         Species = NA,
+         Racemes = NA,
+         Infl_spread = NA,
+         Infl_done = NA,
+         Twist.ties = NA,
+         Q = NA,
+         Note = NA) %>%
+  select(names(read.csv('data/data_active.csv') %>% select(-X))) %>%
+  write.csv('data/data_entry_06-24-2020.csv', na = '', row.names = FALSE)
 
+### 
