4c5a7d5275c419778f607734f19be463b3fee944
swnordstrom <swnordstrom@email.wm.edu>
2021-01-02 15:21:09 -0700

    add some svf plots and exploration

--- analysis/data_exploratoin/radiation_explore.R
+++ analysis/data_exploratoin/radiation_explore.R
@@ -10,7 +10,8 @@ grid.points = read.csv('data/solar_data/elk_coords_svf.csv')
 head(grid.points)
 
 plot.points = read.csv('data/spatial/2020_plot_coords.csv') %>%
-  filter(grepl('^p', Label))
+  filter(grepl('^p', Label)) %>%
+  select(-Z.Altitude)
 
 head(plot.points)
 
@@ -52,4 +53,62 @@ double.merge = plot.points %>%
 nrow(double.merge)
 double.merge
 
+# double.merge %>%
+#   rename(ElementNo_up = ElementNo,
+#          X_East_up = X_East,
+#          Y_North_up = Y_North) %>%
+#   pivot_longer(cols = starts_with(c("ElementNo", "X_East")),
+#                names_to = c("eldir", "xtype"),
+#                names_sep = '-',
+#                values_to = c("elno", "xcoord"))
+#   pivot_longer(cols = starts_with(c("ElementNo", "X_East", "Y_North")),
+#                names_to = c("x", "y", "z"),
+#                names_sep = "-",
+#                values_to = c("a","b", "c"))
 
+solar.plots = double.merge %>%
+  # Get errors introduced by rounding
+  # I.e., how much are each of the grid cells (corners?) off from the coordinate of the plot
+  mutate(err_up = sqrt((X_East - X.Easting)^2 + (Y_North - Y.Northing)^2),
+         err_dn = sqrt((X_East_down - X.Easting)^2 + (Y_North_down - Y.Northing)^2),
+         down_correct = err_dn < err_up) %>%
+  select(-c(X_round, Y_round, X_East, X_East_down, 
+            Y_North, Y_North_down, err_up, err_dn)) %>%
+  gather(c(ElementNo, ElementNo_down), key = 'dir', value = 'ElementNo') %>%
+  mutate(is_down = grepl('down$', dir)) %>%
+  filter(down_correct == is_down) %>%
+  select(-c(down_correct, is_down, dir)) %>%
+  merge(y = grid.points, by = "ElementNo", all.x = TRUE) %>%
+  rename(X_plot = X.Easting, Y_plot = Y.Northing,
+         X_solr = X_East, Y_solr = Y_North) %>%
+  # Rearrange cols
+  # (note: removing elementno here - may be worth keeping if the df expands
+  # to include more info from the solar radiation model)
+  select(Label, svf, X_plot, Y_plot, X_solr, Y_solr, Z_elev)
+
+hist(solar.plots$svf)
+# hmm...
+
+solar.plots %>%
+  ggplot() +
+  geom_point(aes(x = X_plot, y = Y_plot, colour = svf),
+             size = 4)
+
+solar.plots %>%
+  ggplot() +
+  geom_point(aes(x = X_plot, y = Y_plot, colour = svf),
+             size = 4) +
+  scale_color_gradient2(low = 'black', mid = 'white',
+                        high = 'yellow', midpoint = 0.8)
+# hmmm
+
+median(solar.plots$svf)
+# goddamnit
+quantile(solar.plots$svf)
+
+solar.plots %>%
+  mutate(svf.order = order(svf)) %>%
+  ggplot() +
+  geom_point(aes(x = X_plot, y = Y_plot, colour = svf.order),
+             size = 4) +
+  scale_colour_viridis_c(option = 'B')
