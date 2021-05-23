5a857258d88b3765da99084cb14396f9d148db40
swnordstrom <swnordstrom@email.wm.edu>
2021-05-22 18:33:21 -0600

    add final code used in stratification

--- setup/setup_2021/stratify_sampling.R
+++ setup/setup_2021/stratify_sampling.R
@@ -4,6 +4,7 @@
 ##### Setup
 
 library(ggplot2)
+library(purrr)
 library(dplyr)
 library(tidyr)
 
@@ -58,11 +59,11 @@ elk.svf.base +
 
 # Looks good to me
 
-# # Subset svf (for coordinates of ROI)
-# elk.svf.roi = elk.svf %>% 
-#   filter((Easting < 453950) & (Northing > 4431275) & (Northing < 4431450)) %>%
-#   mutate(Easting  = floor(Easting),
-#          Northing = floor(Northing))
+# Subset svf (for coordinates of ROI)
+elk.svf.roi = elk.svf %>%
+  filter((Easting < 453950) & (Northing > 4431275) & (Northing < 4431450)) %>%
+  mutate(Easting  = floor(Easting),
+         Northing = floor(Northing))
 # 
 # coords.roi = elk.coords %>%
 #   filter((X_e < 453950) & (Y_n > 4431275) & (Y_n < 4431450)) %>%
@@ -167,3 +168,351 @@ elk.roi.rank %>%
   ) +
   scale_fill_gradient2(low = 'red', high = 'blue',
                        mid = 'white', midpoint = 0.5)
+
+gauss_rank = gauss_150 %>%
+  arrange(mean.gpl) %>%
+  mutate(gplrank = 1:nrow(.),
+         gpl.res = floor(((gplrank-1) / nrow(.)) * 20) ) #%>%
+
+set.seed(200)
+gauss_rank %>%
+  group_by(gpl.res) %>%
+  sample_n(size = 4) %>%
+  ggplot() +
+  geom_raster(
+    data = gauss_150,
+    aes(x = x_e, y = y_n, fill = mean.gpl),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ),
+    colour = 'red', size = 2
+  ) +
+  scale_fill_viridis_c()
+
+set.seed(299)
+test_strat = gauss_rank %>%
+  group_by(gpl.res) %>%
+  sample_n(size = 4)
+
+test_strat %>%
+  select(gpl.res, x_e, y_n) %>%
+  dist(upper = TRUE, diag = TRUE) %>%
+  as.matrix() %>%
+  apply(1, function(y) min(y[y>0]))
+
+set.seed(299)
+test_strat = gauss_rank %>%
+  group_by(gpl.res) %>%
+  sample_n(size = 4)
+
+liszt = vector('list', 100)
+
+for (trial in 1:100) {
+  
+  liszt[[trial]] = gauss_rank %>%
+    group_by(gpl.res) %>%
+    sample_n(size = 4)
+
+}
+
+lorder = liszt %>% 
+  sapply(function(x) x %>% select(gpl.res, x_e, y_n) %>% dist() %>% sum()) %>%
+  order(decreasing = TRUE)
+
+lorder = liszt %>% 
+  sapply(function(x) x %>% select(gpl.res, x_e, y_n) %>% dist(upper = TRUE, diag = TRUE) %>%
+           as.matrix() %>% apply(1, function(y) min(y[y>0])) %>% sum()) %>%
+  order(decreasing = TRUE)
+
+liszt[[lorder[1]]] %>%
+  ggplot() +
+  geom_raster(
+    data = gauss_rank,
+    aes(x = x_e, y = y_n, fill = gpl.res),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ),
+    colour = 'red', size = 2
+  ) +
+  scale_fill_viridis_c()
+
+liszt[[lorder[3]]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = gpl.res
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+
+#----
+
+# Function for dispersion among groups
+block.matr.max = function(X) {
+  # get distance matrix
+  X %>% ungroup() %>% select(x_e, y_n) %>% dist(upper = TRUE) %>% as.matrix() %>%
+    # wrapper for getting the minimum element
+    mean()
+}
+
+group.dispersion.sum = function(x) {
+  x %>% 
+    # Perform block matrix procedure for each 
+    split(.$gpl.res) %>% 
+    map_dbl(block.matr.max) %>%
+    # get sum across groups
+    sum()
+}
+
+lorder2 = sapply(liszt, group.dispersion.sum) %>% order()
+
+
+liszt[[lorder2[1]]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = mean.gpl
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+
+#---
+
+set.seed(2991)
+test_strat = gauss_rank %>%
+  group_by(gpl.res) %>%
+  sample_n(size = ifelse(gpl.res < 10, 6, 4))
+
+list100 = vector('list', 100)
+
+for (trial in 1:100) {
+  
+  list100[[trial]] = gauss_rank %>%
+    group_by(gpl.res) %>%
+    sample_n(size = ifelse(gpl.res < 10, 6, 4))
+  
+}
+
+
+liszt[[1]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = mean.gpl
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+
+clorder = sapply(list100, group.dispersion.sum) %>% order()
+
+liszt[[clorder[1]]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = mean.gpl
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+
+### Actual stratification:
+
+gauss.gpl = read.csv('setup/setup_2021/spatial_pathlength_gauss150.csv')
+
+## Do stratification
+
+# Get rank of mean gpl for each point in ROI
+gauss.rank = gauss.gpl %>%
+  select(-X) %>%
+  arrange(mean.gpl) %>%
+  mutate(gplrank = 1:nrow(.),
+         gpl.res = floor(((gplrank-1) / nrow(.)) * 20) )
+
+set.seed(2991)
+
+# Object to store random samples
+list.strats = vector('list', 100)
+
+# Do stratified sampling routine (100 times)
+for (trial in 1:100) {
+  
+  list.strats[[trial]] = gauss.rank %>%
+    group_by(gpl.res) %>%
+    sample_n(size = ifelse(gpl.res < 5, 6, 4))
+  
+}
+
+## Wrapper functions for gettng disperson
+
+# Function for dispersion among groups
+block.matr.max = function(X) {
+  # get distance matrix
+  X %>% ungroup() %>% select(x_e, y_n) %>% dist(upper = TRUE) %>% as.matrix() %>%
+    # wrapper for getting the minimum element
+    mean()
+}
+
+group.dispersion.sum = function(x) {
+  x %>% 
+    # Perform block matrix procedure for each 
+    split(.$gpl.res) %>% 
+    map_dbl(block.matr.max) %>%
+    # get sum across groups
+    sum()
+}
+
+# Get dispersion rank for each sample
+
+lorder = sapply(list.strats, group.dispersion.sum) %>% order()
+
+# Plot to inspect
+# Plotting most dispersed
+list.strats[[lorder[1]]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = mean.gpl
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+
+# Trying second, third, etc...
+list.strats[[lorder[3]]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = mean.gpl
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+# I like this one
+
+list.strats[[lorder[3]]] %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n
+    ), 
+    size = 5, colour = 'white'
+  ) +
+  geom_point(
+    aes(
+      x = x_e, y = y_n, colour = mean.gpl
+    ), 
+    size = 4
+  ) +
+  scale_color_viridis_c(option = 'A') +
+  scale_fill_viridis_c()
+# I like this one
+
+plot.positions = list.strats[[lorder[3]]]
+
+plot.positions %>%
+  ggplot() +
+  geom_raster(
+    data = elk.svf.roi,
+    aes(x = Easting, y = Northing, fill = Svf),
+    alpha = 0.8
+  ) +
+  geom_label(
+    aes(
+      x = x_e, y = y_n, label = round(mean.gpl, 2)
+    ), 
+    size = 2, fill = 'white'
+  ) +
+  scale_fill_gradient(low = 'black', high = 'white') +
+  theme_bw() +
+  theme(legend.position = 'none') +
+  ggsave('setup/setup_2021/plot_raw_full.png', width = 11, height = 8)
+
+write.csv(plot.positions, row.names = FALSE,
+          'setup/setup_2021/plot_init_coords.csv')
+
