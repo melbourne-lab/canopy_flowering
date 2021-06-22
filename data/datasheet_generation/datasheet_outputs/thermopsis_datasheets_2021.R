ba42e8622f401c6bc4bcb12d0577c5ff3ff7838d
swnordstrom <swnordstrom@email.wm.edu>
2021-06-21 19:23:29 -0600

    slightly more organization for jun 21 datasheet generation

--- data/datasheet_generation/datasheet_outputs/thermopsis_datasheets_2021.R
+++ data/datasheet_generation/datasheet_outputs/thermopsis_datasheets_2021.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(tidyr)
 
+### 21 Jun 2021
+
 # Create datasheet for 21 Jun 2021
 
 read.csv('data/raw_data/data_2021/Thermopsis_newplants_2021.csv') %>%
@@ -19,3 +21,12 @@ read.csv('data/raw_data/data_2021/Thermopsis_newplants_2021.csv') %>%
   write.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2021/therm_21-06-2021.csv',
             na = '', row.names = FALSE)
 
+# for data entry
+
+read.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2021/therm_21-06-2021.csv') %>%
+  mutate(Date = NA, Note = NA) %>%
+  select(Date, Plot, Tag, Toothpick, Q, Fl_stems, Fl_open, Fl_done, Note) %>%
+  rename(Tp = Toothpick, Fl_Stems = Fl_stems, Fl_Open = Fl_open, Fl_Done = Fl_done) %>%
+  write.csv(file = 'data/raw_data/data_2021/therm_entry_21-06-2021.csv', na = '', row.names = FALSE)
+
+### 
