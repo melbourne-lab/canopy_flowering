2a42a90edb663ca72948bf5f53b3f7b27969269a
Scott Watson Nordstrom <scno5885@librcomm-04.colorado.edu>
2021-06-23 14:09:14 -0600

    add code and csv for flowering plants 2021

--- data/datasheet_generation/datasheet_outputs/thermopsis_datasheets_2021.R
+++ data/datasheet_generation/datasheet_outputs/thermopsis_datasheets_2021.R
@@ -47,3 +47,36 @@ sample(plots.0621$Plot, size = 12, replace = FALSE)
 # Slight reorder:
 # 37, 8, 6, 10, 7, 74, 71, 76, 73, 74, 62, 59
 
+### 24 Jun 2021
+
+# Get (flowering/budding) plants to check
+
+read.csv('data/raw_data/data_2021/Thermopsis_newplants_2021.csv') %>%
+  rename(Fl_Done = Fl_done, Fl_Open = Fl_open, Fl_Stems = Fl_stems) %>%
+  rbind(read.csv('data/raw_data/data_2021/therm_entry_21-06-2021.csv')) %>%
+  # Get rid of plants without tags (none were flowering as of last visit)
+  filter(!is.na(Tag)) %>%
+  # Get only most recent record for each plant
+  arrange(Plot, Tag, desc(Date)) %>%
+  distinct(Plot, Tag, .keep_all = TRUE) %>%
+  # Reformat columns for a datasheet
+  # rename(old_stems = Fl_stems, old_open = Fl_open, Old_note = note) %>%
+  mutate(Old_info = paste(Fl_Stems, Fl_Open, Fl_Done, sep = ';'),
+         Fl_stems = NA, Fl_open = NA, Fl_done = NA, q = NA) %>%
+  rename(Old_note = Note, Last_date = Date, Old_q = Q, Q = q) %>%
+  select(Plot, Tag, Toothpick, Q, Fl_stems, Fl_open, Fl_done,
+         Last_date, Old_info, Old_q, Old_note) %>%
+  # Export CSV
+  write.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2021/therm_24-06-2021.csv',
+            na = '', row.names = FALSE)
+
+# Get toothpick list (may be helpful)
+
+read.csv('data/raw_data/data_2021/Thermopsis_newplants_2021.csv') %>%
+  rename(Fl_Done = Fl_done, Fl_Open = Fl_open, Fl_Stems = Fl_stems) %>%
+  rbind(read.csv('data/raw_data/data_2021/therm_entry_21-06-2021.csv')) %>%
+  group_by(Plot, Toothpick) %>%
+  filter(all(is.na(Tag))) %>%
+  filter(!any(grepl('collected', Note))) %>%
+  distinct(Plot, Toothpick)
+
