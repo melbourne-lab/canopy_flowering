bc8b8836712559596a2c6f4bfd5a1199a93d2789
swnordstrom <swnordstrom@email.wm.edu>
2021-07-15 06:35:09 -0600

    add plots from first week of data

--- sensors_and_loggers/sandbox/visualize_test_data.R
+++ sensors_and_loggers/sandbox/visualize_test_data.R
@@ -150,3 +150,70 @@ p48 %>%
 
 ggsave(filename = '~/Desktop/plot_48_jul3-jul5_soiltemp.pdf')
   
+### Compare soil moisture between plots 25 and 48 (only two working plots...)
+
+pl48 = read.csv('sensors_and_loggers/data/plot48_2021-07-05_2021-07-11_raw.TXT') %>%
+  mutate(Hour_MDT = Hour_UTC - 6) %>%
+  mutate(Day_MDT  = Day_UTC - as.numeric(Hour_MDT < 0),
+         Hour_MDT = Hour_MDT +24* as.numeric(Hour_MDT < 0)) %>%
+  mutate(time.c = chron::as.times(paste(Hour_MDT, Minute, Second, sep = ":")))
+pl25 = read.csv('sensors_and_loggers/data/plot25_2021-07-03_2021-07-11_raw.TXT') %>%
+  mutate(Hour_MDT = Hour_UTC - 6) %>%
+  mutate(Day_MDT  = Day_UTC - as.numeric(Hour_MDT < 0),
+         Hour_MDT = Hour_MDT +24* as.numeric(Hour_MDT < 0)) %>%
+  mutate(time.c = chron::as.times(paste(Hour_MDT, Minute, Second, sep = ":")))
+
+mois.combined = rbind(
+  pl48 %>% filter(grepl('mois', Reading_type)) %>% mutate(Plot = 48),
+  pl25 %>% filter(grepl('mois', Reading_type)) %>% mutate(Plot = 25)
+) %>%
+  mutate(VWC = (3.879e-4)*Reading_raw - 0.6956) 
+
+mois.combined %>%
+  ggplot(aes(x = time.c, y = VWC)) +
+  geom_line(aes(group = Plot, colour = factor(Plot))) +
+  facet_wrap(~ paste('July ', Day_MDT))
+
+ggsave('sensors_and_loggers/sandbox/compare_vwc.pdf')
+
+temp.combined = rbind(
+  pl48 %>% filter(grepl('T11', Sensor_ID), grepl('temp', Reading_type)) %>% mutate(Plot = 48),
+  pl25 %>% filter(grepl('T11', Sensor_ID), grepl('temp', Reading_type)) %>% mutate(Plot = 25)
+)
+
+temp.combined %>%
+  ggplot(aes(x = time.c, y = Reading_raw)) +
+  geom_line(aes(group = Plot, colour = factor(Plot))) +
+  facet_wrap(~ paste('July ', Day_MDT))
+
+ggsave('sensors_and_loggers/sandbox/compare_soil_temp.pdf')
+
+calibration.factors = merge(
+  x = read.csv('sensors_and_loggers/sensor_logger_masterlist_2021.csv'),
+  y = read.csv('sensors_and_loggers/SP510_calibration_factors.csv'),
+  by.x = 'SP510', by.y = 'Sensor_ID'
+) %>%
+  select(SP510, TEROS, Plot, Calibration_factor_W_m..2_per_mV) %>%
+  rename(Calibration_scalar = Calibration_factor_W_m..2_per_mV)
+
+wm2.combined = rbind(
+  pl48 %>% filter(grepl('pyra', Sensor_ID), grepl('volt', Reading_type)) %>% mutate(Plot = 48),
+  pl25 %>% filter(grepl('pyra', Sensor_ID), grepl('volt', Reading_type)) %>% mutate(Plot = 25)
+) %>%
+  merge(y = calibration.factors, by = 'Plot') %>%
+  mutate(Wm2 = Reading_raw * Calibration_scalar)
+
+wm2.combined %>%
+  ggplot(aes(x = time.c, y = Wm2)) +
+  geom_line(aes(group = Plot, colour = factor(Plot))) +
+  facet_wrap(~ paste('July ', Day_MDT))
+
+ggsave('sensors_and_loggers/sandbox/compare_wm2.pdf')
+
+battery.combined = rbind(
+  pl48 %>% filter(grepl('batt', Sensor_ID)) %>% mutate(Plot = 48),
+  pl25 %>% filter(grepl('batt', Sensor_ID)) %>% mutate(Plot = 25)
+)
+
+ggplot(battery.combined, aes(x = Unixtime_UTC, y = Reading_raw)) +
+  geom_line(aes(group = Plot, colour = factor(Plot)))
