289326c86b4a86b47682805de450d28211510cc5
swnordstrom <swnordstrom@email.wm.edu>
2021-07-21 12:39:08 -0600

    init campanula files for july 22

--- data/datasheet_generation/campanula_datasheets_2021.R
+++ data/datasheet_generation/campanula_datasheets_2021.R
@@ -36,4 +36,23 @@ sample(plots.0720$Plot[!plots.0720$nfl])
 sample(plots.0720$Plot[plots.0720$nfl > 0], size = 4)
 # [1] 53 73 71  6
 
+### July 22 2021
+
+# get phen datasheet
+read.csv('data/raw_data/data_2021/Campanula_newplants_2021.csv') %>%
+  rbind(read.csv('data/raw_data/data_2021/campa_entry_19-07-21.csv')) %>%
+  arrange(Plot, Tag) %>%
+  mutate(Prev = paste(Fl_stems, Fl_open, Fl_done, sep = ';')) %>%
+  rename(Prev_note = Note) %>%
+  mutate(Date = NA, Fl_stems = NA, Fl_open = NA, Fl_done = NA, Q = NA) %>%
+  select(Date, Plot, Tag, Fl_stems, Fl_open, Fl_done, Q, Prev, Prev_note) %>%
+  write.csv(file = 'data/datasheet_generation/datasheet_outputs/data_datasheets_2021/campa_22-07-21.csv',
+            na = '', row.names = FALSE)
+
+# get data entry csv
+
+read.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2021/campa_22-07-21.csv') %>%
+  mutate(Date = NA, Page = NA, Note = NA) %>%
+  select(Date, Plot, Tag, Fl_stems, Fl_open, Fl_done, Q, Page, Note) %>%
+  write.csv('data/raw_data/data_2021/campa_entry_22-07-21.csv', row.names = FALSE, na = '')
 
