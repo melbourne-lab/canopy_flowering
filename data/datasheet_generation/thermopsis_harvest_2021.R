603e2f8b78486d2ff7b6a3d03fd568cd1061601e
swnordstrom <swnordstrom@email.wm.edu>
2021-07-21 22:38:31 -0600

    thermopsis harvest datasheets (is csv in right place...?)

--- data/datasheet_generation/thermopsis_harvest_2021.R
+++ data/datasheet_generation/thermopsis_harvest_2021.R
@@ -112,3 +112,9 @@ read.csv('data/raw_data/data_2021/Thermopsis_newplants_2021.csv') %>%
   arrange(Plot) %>%
   select(-Return) %>%
   write.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2021/therm_harvest_19-07-21.csv', na = '', row.names = FALSE)
+
+# sheet for entry
+read.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2021/therm_harvest_19-07-21.csv') %>%
+  mutate(Return = NA, Page = NA) %>%
+  select(Harvest_Date, Plot, Tag, Fruit_Pres, Fruit_Coll, Return, Page, Harvest_Note) %>%
+  write.csv('data/raw_data/data_2021/therm_harvest_19-07-21.csv', na = '', row.names = FALSE)
