88a177b33a3940dacf22c3136309e26921da64bb
swnordstrom <swnordstrom@email.wm.edu>
2022-05-19 11:25:55 -0600

    visualize ref times, process, export final 162 hrs of ref times

--- sensors_and_loggers/sandbox/assess_ref_sensors_2021.R
+++ sensors_and_loggers/sandbox/assess_ref_sensors_2021.R
@@ -64,15 +64,162 @@ ref.sens %>%
   geom_line(aes(group = Day_MDT))
 # interesting/winteresting
 
+# Series in July
+ref.sens %>%
+  filter(Month_UTC %in% 7) %>%
+  filter(plot %in% 'plot6') %>%
+  mutate(time = Hour_MDT + Minute/60) %>%
+  ggplot(aes(x = time, y = volt)) +
+  # geom_segment(data = . %>% filter(!Minute),
+  #              aes(x = time, xend = time,
+  #                  y = 0, yend = 60),
+  #              size = 0.25, linetype = 3) +
+  geom_line() +
+  scale_x_continuous(breaks = 8:17) +
+  facet_wrap(~ Day_MDT) +
+  theme(axis.text.x = element_text(angle = 90),
+        panel.grid.minor = element_blank())
+
+
+# Times in August
 ref.sens %>%
   filter(Month_UTC %in% 8) %>%
   filter(plot %in% 'plot6') %>%
   mutate(time = Hour_MDT + Minute/60) %>%
   ggplot(aes(x = time, y = volt)) +
+  # geom_segment(data = . %>% filter(!Minute),
+  #              aes(x = time, xend = time,
+  #                  y = 0, yend = 60),
+  #              size = 0.25, linetype = 3) +
   geom_line() +
-  facet_wrap(~ Day_MDT)
+  scale_x_continuous(breaks = 9:17) +
+  facet_wrap(~ Day_MDT) +
+  theme(axis.text.x = element_text(angle = 90),
+        panel.grid.minor = element_blank())
 
+# Times in September
 ref.sens %>%
-  filter(Month_UTC %in% 8, plot %in% 'plot6') %>%
-  group_by(Day_MDT, Hour_MDT, Half_hour = Min %/% 30) %>%
+  filter(Month_UTC %in% 9) %>%
+  filter(plot %in% 'plot6') %>%
+  mutate(time = Hour_MDT + Minute/60) %>%
+  ggplot(aes(x = time, y = volt)) +
+  # geom_segment(data = . %>% filter(!Minute),
+  #              aes(x = time, xend = time,
+  #                  y = 0, yend = 60),
+  #              size = 0.25, linetype = 3) +
+  geom_line() +
+  scale_x_continuous(breaks = 9:17) +
+  facet_wrap(~ Day_MDT) +
+  theme(axis.text.x = element_text(angle = 90),
+        panel.grid.minor = element_blank())
+
+# Times in October
+ref.sens %>%
+  filter(Month_UTC %in% 10) %>%
+  filter(plot %in% 'plot6') %>%
+  mutate(time = Hour_MDT + Minute/60) %>%
+  ggplot(aes(x = time, y = volt)) +
+  # geom_segment(data = . %>% filter(!Minute),
+  #              aes(x = time, xend = time,
+  #                  y = 0, yend = 60),
+  #              size = 0.25, linetype = 3) +
+  geom_line() +
+  scale_x_continuous(breaks = 9:17) +
+  facet_wrap(~ Day_MDT) +
+  theme(axis.text.x = element_text(angle = 90),
+        panel.grid.minor = element_blank())
+
+### Read in reference times
+
+ref.times = read.csv('sensors_and_loggers/processed_data/sensor_reference_times_2021.csv')
+# all times are MDT
+# Half_hour is NA is full hour is usable, 0/30 if only a respective half hour is usable
+
+# Add in half_hours for those that are whole hour-usable
+ref.times = ref.times %>%
+  filter(is.na(Half_hour)) %>%
+  select(-Half_hour) %>%
+  mutate(Half_hour0 = 1, Half_hour30 = 1) %>%
+  gather(key = Half_hour, value = val, -c(Month, Day, Hour)) %>%
+  mutate(Half_hour = as.numeric(gsub('Half\\_hour', '', Half_hour))) %>%
+  select(-val) %>%
+  rbind(ref.times %>% filter(!is.na(Half_hour))) %>%
+  arrange(Month, Day, Hour, Half_hour)
+
+# write.csv(file = 'sensors_and_loggers/processed_data/processed_ref_times_2021.csv',
+#           row.names = FALSE,
+#           x = ref.times)
+
+### Plot these to make sure they are okay
+
+# First, need to get the half hour from the ref sensor readings
+ref.sens.times = ref.sens %>%
+  ungroup() %>%
+  rename(Month = Month_UTC,
+         Day   = Day_MDT,
+         Hour  = Hour_MDT) %>%
+  mutate(Half_hour = 30 * as.numeric(Minute < 30)) %>%
+  # Now merge, making sure to include all x
+  merge(y = ref.times, all.x = FALSE, all.y = TRUE)
+
+nrow(ref.sens.times)
+# Hell yeah
+
+# Now plot
+ref.sens.times %>%
+  mutate(time = Hour + (Minute / 60)) %>%
+  ggplot(aes(x = time, y = volt)) +
+  geom_line(aes(group = interaction(Month, Day, Hour, Half_hour)),
+            size = 0.5) +
+  scale_x_continuous(breaks = 8:17)
+# okay some of these are bad
+
+# Can use range() to catch some of these no?
+# range of readings in a half hour shouldn't be greater than 10
+# (lol this would have been a good screen anyway...)
+ref.sens.times %>%
+  group_by(Month, Day, Hour, Half_hour) %>%
+  summarise(voltage.range = diff(range(volt))) %>%
+  ggplot(aes(x = voltage.range)) +
+  geom_histogram(binwidth = 2)
+# 6 or 8 as a cutoff
+
+ref.sens.times %>%
+  group_by(Month, Day, Hour, Half_hour) %>%
+  mutate(voltage.range = diff(range(volt))) %>%
+  ungroup() %>%
+  mutate(time = Hour + (Minute / 60)) %>%
+  ggplot(aes(x = time, y = volt)) +
+  geom_line(aes(group  = interaction(Month, Day, Hour, Half_hour),
+                colour = voltage.range > 5),
+            size = 0.5) +
+  scale_x_continuous(breaks = 8:17)
   
+# 5 looks like a good cutoff
+
+ref.sens.times = ref.sens.times %>%
+  group_by(Month, Day, Hour, Half_hour) %>%
+  filter(diff(range(volt)) < 5) %>%
+  ungroup()
+
+nrow(ref.sens.times)
+
+merge(x = ref.times, y = ref.sens.times,
+      all.x = FALSE, all.y = TRUE) %>%
+  nrow()
+
+# 9720 minutes... still pretty good
+
+# Export modified ref times list
+merge(x = ref.times, y = ref.sens.times,
+      all.x = FALSE, all.y = TRUE) %>%
+  write.csv(file = 'sensors_and_loggers/processed_data/processed_ref_times_2021.csv',
+            row.names = FALSE)
+
+ref.sens.times %>%
+  mutate(time = Hour + (Minute / 60)) %>%
+  ggplot(aes(x = time, y = volt)) +
+  geom_line(aes(group = interaction(Month, Day, Hour, Half_hour)),
+            size = 0.5) +
+  scale_x_continuous(breaks = 8:17)
+# okay some of these are bad
