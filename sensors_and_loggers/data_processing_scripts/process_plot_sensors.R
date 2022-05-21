81215bb355e40660e5e01f94a2a1c4bdc14ba864
swnordstrom <swnordstrom@email.wm.edu>
2022-05-20 19:01:22 -0600

    scripts for final couple of sensors

--- sensors_and_loggers/data_processing_scripts/process_plot_sensors.R
+++ sensors_and_loggers/data_processing_scripts/process_plot_sensors.R
@@ -21,7 +21,7 @@ ref.times = read.csv('sensors_and_loggers/processed_data/processed_ref_times_202
 # - Plot 25: note that there was an SP510 replacement during this time
 # - Plot 33: the timer here got messed up - multiple times! - so Aug.
 #            measurements will need adjustments
-# 
+# - Need to add the remaining October observations
 
 #################################################################
 ##### Plot 2 ####################################################
@@ -765,3 +765,115 @@ write.csv(this.plot.data,
 #################################################################
 ##### Plot 75 ###################################################
 #################################################################
+
+this.plot.data = paste0(sensor.dir, grep('plot75\\_', dir(sensor.dir), value = TRUE)) %>%
+  grep(pattern = 'raw', x = ., value = TRUE) %>%
+  lapply(function(x) read.csv(x) %>% mutate(fn = x)) %>%
+  do.call(what = rbind) %>%
+  # Get only pyranometer readings
+  filter(Sensor_ID %in% c('pyran')) %>%
+  spread(key = Reading_type, value = Reading_raw) %>%
+  # Add correct time
+  mutate(Hour = (Hour_UTC - 6) %% 24,
+         Day  = Day_UTC - as.logical(Hour > Hour_UTC)) %>%
+  # Filter to only daylight hours
+  filter(Hour %in% 8:17) %>%
+  # Rename month column
+  rename(Month = Month_UTC) %>%
+  # Add proper plot name (might not be needed)
+  mutate(fn = gsub('sensors\\_and\\_loggers\\/data\\/', '', fn),
+         fn = gsub('\\-', '', fn),
+         fn = gsub('\\_\\d{8}|\\_raw\\.TXT', '', fn)) %>%
+  # Get rid of unnecessary columns
+  select(-c(Hour_UTC, Day_UTC, Sensor_ID, ` bits`)) %>%
+  # Prepare voltage
+  rename(volt = ` volt`) %>%
+  group_by(Month, Day, Hour, Minute, fn) %>% 
+  summarise(volt = mean(as.numeric(volt)))
+
+head(this.plot.data)
+nrow(this.plot.data)
+
+this.plot.data %>%
+  mutate(time = Hour + Minute/60,
+         Date = as.Date(paste0('2021-', Month, '-', Day), format = '%Y-%m-%d')) %>%
+  ggplot(aes(x = time, y = volt)) +
+  geom_line() +
+  facet_wrap(~ Date)
+
+# Ah this is the one that got knocked over a lot
+
+this.plot.data = this.plot.data %>%
+  # Remove July 13 - 14
+  filter(!(Month %in% 7 & Day %in% 13:14)) %>%
+  # Remove July 23 - 26
+  filter(!(Month %in% 7 & Day %in% 24:26))
+
+# some other times that look not great, but this honestly could just be cloud
+
+# Gewt only clearsky readings
+this.plot.data = merge(
+  x = this.plot.data %>% mutate(Half_hour = 30 * as.numeric(Minute < 30)),
+  y = ref.times %>% distinct(Month, Day, Hour, Half_hour),
+  all.x = FALSE, all.y = FALSE
+)
+
+nrow(this.plot.data)
+
+write.csv(this.plot.data,
+          file = 'sensors_and_loggers/processed_data/plot75_pyran_clearsky_2021.csv',
+          row.names = FALSE)
+
+
+#################################################################
+##### Plot 77 ###################################################
+#################################################################
+
+this.plot.data = paste0(sensor.dir, grep('plot77\\_', dir(sensor.dir), value = TRUE)) %>%
+  grep(pattern = 'raw', x = ., value = TRUE) %>%
+  lapply(function(x) read.csv(x) %>% mutate(fn = x)) %>%
+  do.call(what = rbind) %>%
+  # Get only pyranometer readings
+  filter(Sensor_ID %in% c('pyran')) %>%
+  spread(key = Reading_type, value = Reading_raw) %>%
+  # Add correct time
+  mutate(Hour = (Hour_UTC - 6) %% 24,
+         Day  = Day_UTC - as.logical(Hour > Hour_UTC)) %>%
+  # Filter to only daylight hours
+  filter(Hour %in% 8:17) %>%
+  # Rename month column
+  rename(Month = Month_UTC) %>%
+  # Add proper plot name (might not be needed)
+  mutate(fn = gsub('sensors\\_and\\_loggers\\/data\\/', '', fn),
+         fn = gsub('\\-', '', fn),
+         fn = gsub('\\_\\d{8}|\\_raw\\.TXT', '', fn)) %>%
+  # Get rid of unnecessary columns
+  select(-c(Hour_UTC, Day_UTC, Sensor_ID, ` bits`)) %>%
+  # Prepare voltage
+  rename(volt = ` volt`) %>%
+  group_by(Month, Day, Hour, Minute, fn) %>% 
+  summarise(volt = mean(as.numeric(volt)))
+
+head(this.plot.data)
+nrow(this.plot.data)
+
+this.plot.data %>%
+  mutate(time = Hour + Minute/60,
+         Date = as.Date(paste0('2021-', Month, '-', Day), format = '%Y-%m-%d')) %>%
+  ggplot(aes(x = time, y = volt)) +
+  geom_line() +
+  facet_wrap(~ Date)
+# possibly fine
+
+# Gewt only clearsky readings
+this.plot.data = merge(
+  x = this.plot.data %>% mutate(Half_hour = 30 * as.numeric(Minute < 30)),
+  y = ref.times %>% distinct(Month, Day, Hour, Half_hour),
+  all.x = FALSE, all.y = FALSE
+)
+
+nrow(this.plot.data)
+
+write.csv(this.plot.data,
+          file = 'sensors_and_loggers/processed_data/plot77_pyran_clearsky_2021.csv',
+          row.names = FALSE)
