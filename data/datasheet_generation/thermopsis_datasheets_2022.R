e6987fb56f1515b95996acda07cd54f71333abfc
swnordstrom <swnordstrom@email.wm.edu>
2022-06-28 22:18:14 -0600

    thermopsis data prep for june 27 (no entry yet)

--- data/datasheet_generation/thermopsis_datasheets_2022.R
+++ data/datasheet_generation/thermopsis_datasheets_2022.R
@@ -130,3 +130,65 @@ read.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2022/therm
   mutate(Date = NA, Notes = NA, Page = NA) %>%
   select(Date, Plot, Tag, N_infl, Fl_open, Fl_done, Q, Page, Notes) %>%
   write.csv(file = 'data/raw_data/data_2022/therm_entry_06-23-2022.csv', na = '', row.names = FALSE)
+
+### Jun 27
+
+### Jun 23 2022
+
+# Make data entry datasheet
+read.csv('data/raw_data/data_2022/Thermopsis_newplants_2022.csv') %>%
+  rbind(
+    read.csv('data/raw_data/data_2022/therm_entry_06-13-2022.csv'),
+    read.csv('data/raw_data/data_2022/therm_entry_06-16-2022.csv'),
+    read.csv('data/raw_data/data_2022/therm_entry_06-20-2022.csv'),
+    read.csv('data/raw_data/data_2022/therm_entry_06-23-2022.csv')
+  ) %>%
+  arrange(Plot, Tag, desc(Date)) %>%
+  group_by(Plot, Tag) %>%
+  # Mark finished plants
+  mutate(done = (!Fl_open[1] & !Fl_open[2]) & 
+           (Fl_done[1] <= Fl_done[2]) & 
+           !(grepl('yv', Notes[1]) | grepl('yv', Notes[2])),
+         done = ifelse(is.na(done), FALSE, done),
+         done = ifelse(done, 'done', NA)) %>%
+  # Filter out plants with two can't find records
+  group_by(Plot, Tag) %>%
+  filter(sum(grepl("can\'t find", Notes)) < 2) %>%
+  ungroup() %>%
+  distinct(Tag, .keep_all = TRUE) %>%
+  # Done tag...
+  arrange(Plot, !is.na(done), Tag) %>%
+  # Get rid of done plants older than a week old
+  filter(as.Date(Date, '%m/%d/%y') > as.Date('6/18/22', '%m/%d/%y')) %>%
+  # Reformat columns for a datasheet
+  # rename(old_stems = Fl_stems, old_open = Fl_open, Old_note = note) %>%
+  mutate(Old_info = paste(N_infl, Fl_open, Fl_done, sep = ';'),
+         Fl_open = NA, Fl_done = NA, N_infl = NA, q = NA) %>%
+  rename(Old_notes = Notes, Last_date = Date, Old_q = Q, Q = q, Done = done) %>%
+  select(Plot, Tag, Q, N_infl, Fl_open, Fl_done, Done, 
+         Last_date, Old_info, Old_q, Old_notes) %>%
+  # Export CSV
+  write.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2022/therm_06-27-2022.csv',
+            na = '', row.names = FALSE)
+
+set.seed(2043119)
+
+# for pollen addition
+read.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2022/therm_06-27-2022.csv') %>%
+  select(Plot, Tag) %>%
+  sample_n(30) %>%
+  mutate(i = 1:30) %>%
+  mutate(Treatment = ifelse(i %% 2, 'Addition', 'Control'),
+         Extra     = ifelse(i < 25, '', paste0('Extra', i - 24))) %>%
+  select(-i) %>%
+  arrange(Plot, Tag, Extra) %>%
+  mutate(Treated = NA, Flowers_treated = NA, Flowers = NA, Note = NA) %>%
+  write.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2022/pollen_add_06-27-2022.csv',
+            na = '', row.names = FALSE)
+
+# data entry
+read.csv('data/datasheet_generation/datasheet_outputs/data_datasheets_2022/therm_06-27-2022.csv') %>%
+  filter(Done %in% '') %>%
+  mutate(Date = NA, Notes = NA, Page = NA) %>%
+  select(Date, Plot, Tag, N_infl, Fl_open, Fl_done, Q, Page, Notes) %>%
+  write.csv(file = 'data/raw_data/data_2022/therm_entry_06-27-2022.csv', na = '', row.names = FALSE)
